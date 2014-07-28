1cafb254d8078a957557ec99694b3decbfe50619
Raymond Cheng <ryscheng@cs.washington.edu>
2014-07-27 22:14:10 -0700

    adding release scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,7 +92,27 @@ module.exports = function (grunt) {
         }
       }
     },
-    gitinfo: {}
+    bump: {
+      options: {
+        files: ['package.json'],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin'
+      }
+    },
+    'npm-publish': {
+      options: {
+        // list of tasks that are required before publishing
+        requires: [],
+        // if the workspace is dirty, abort publishing (to avoid publishing local changes)
+        abortIfDirty: true,
+      }
+    }
   });
 
   // Load tasks.
@@ -100,6 +120,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-npm');
+  grunt.loadNpmTasks('grunt-bump');
   
   // Default tasks.
   grunt.registerTask('build', [
@@ -117,6 +139,16 @@ module.exports = function (grunt) {
   grunt.registerTask('demo', [
     'connect:demo',
   ]);
+
+  grunt.registerTask('release', function(arg) {
+    if (arguments.length === 0) {
+      arg = 'patch';
+    }
+    grunt.task.run([
+      'bump:'+arg,
+      'npm-publish'
+    ]);
+  });
 
   grunt.registerTask('default', ['build']);
 };
